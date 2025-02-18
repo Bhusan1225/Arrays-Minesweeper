a9cc3676e5e2c96597829b8029193fefa4296281
bhusanSahoo <bhusan12sahoo@gmail.com>
2025-02-18 09:35:26 +0530

    .

--- Array-Minesweeper/source/Gobal/ServiceLocator.cpp
+++ Array-Minesweeper/source/Gobal/ServiceLocator.cpp
@@ -78,7 +78,7 @@ namespace Global
 
 	UIService* ServiceLocator::getUIService() { return ui_service; }
 
-	BoardService* ServiceLocator::getBoardService() { return board_service; }
+	BoardService* ServiceLocator::getBoardService() { return board_service; } //added the board service in the service locator
 
 	void ServiceLocator::deleteServiceLocator() { delete(this); }
 }
